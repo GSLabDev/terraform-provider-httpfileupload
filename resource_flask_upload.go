91713198e3f5c81c59a7c1ec1571b9f9f34cfbd0
Payal <payal.rahatal@gslab.com>
2017-06-20 15:31:45 +0530

    upload and delete file using http request

--- resource_flask_upload.go
+++ resource_flask_upload.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-    	"github.com/hashicorp/terraform/helper/schema"
-    	"os"
-        "net/http"
-        "io/ioutil"
-        "bytes"
-        "fmt"
-        "io"
-        "mime/multipart"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func resourceUpload() *schema.Resource {
@@ -17,20 +22,19 @@ func resourceUpload() *schema.Resource {
 		Read:   resourceUploadRead,
 		Update: resourceUploadUpdate,
 		Delete: resourceUploadDelete,
-
 		Schema: map[string]*schema.Schema{
-			"ip_address": &schema.Schema{
+			"host_url": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("IP_ADDR", nil),
-				Description: descriptions["ip_address"],
+				Description: descriptions["host_url"],
 			},
 
-			"file_name": &schema.Schema{
-				Type:           schema.TypeString,
-				Required:       true,
-				DefaultFunc:    schema.EnvDefaultFunc("FILENAME", ""),
-				Description:    descriptions["file_name"],
+			"file_path": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("FILENAME", ""),
+				Description: descriptions["file_path"],
 			},
 		},
 	}
@@ -40,52 +44,47 @@ var descriptions map[string]string
 
 func init() {
 	descriptions = map[string]string{
-		"ip_address": "The ip address of the machine you want to upload file to.",
+		"host_url": "The ip address of the machine you want to upload file to.",
 
-		"file_name": "Entire filepath including the filename.",
+		"file_path": "Entire filepath including the filename.",
 	}
 }
 
-
 func resourceUploadCreate(d *schema.ResourceData, m interface{}) error {
-	ip_address := d.Get("ip_address").(string)
-	d.SetId(ip_address)
-	filename := d.Get("file_name").(string)
-	d.SetId(filename)
-        targetUrl:="http:/"+"/"+ip_address+":8080/upload"
+	hostUrl := d.Get("host_url").(string)
+	filePath := d.Get("file_path").(string)
+	_, file := filepath.Split(filePath)
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+	fileWriter, err := bodyWriter.CreateFormFile("file", file)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return fmt.Errorf("[ERROR] Error in writing to buffer %s", err)
+	}
+	fh, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("error opening file")
+		return fmt.Errorf("[ERROR] Error while opening file %s ", err)
+	}
 
-
-        bodyBuf := &bytes.Buffer{}
-        bodyWriter := multipart.NewWriter(bodyBuf)
-        fileWriter, err := bodyWriter.CreateFormFile("file", filename)
-        if err != nil {
-            fmt.Println("error writing to buffer")
-            return fmt.Errorf("[ERROR] Error in writing to buffer %s",err)
-         }
-       fh, err := os.Open(filename)
-       if err != nil {
-          fmt.Println("error opening file")
-          return fmt.Errorf("[ERROR] Error while opening file %s ", err)
-        }
-
-        _, err = io.Copy(fileWriter, fh)
-        if err != nil {
-           return err
-          }
-       contentType := bodyWriter.FormDataContentType()
-       bodyWriter.Close()
-       resp, err := http.Post(targetUrl, contentType, bodyBuf)
-       if err != nil {
-          return fmt.Errorf("[ERROR] Request failed")
-         }
-       defer resp.Body.Close()
-       resp_body, err := ioutil.ReadAll(resp.Body)
-       if err != nil {
-          return err
-        }
-       fmt.Println(resp.Status)
-       fmt.Println(string(resp_body))
-       return nil
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		return err
+	}
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+	resp, err := http.Post(hostUrl, contentType, bodyBuf)
+	if err != nil {
+		return fmt.Errorf("[ERROR] Request failed")
+	}
+	defer resp.Body.Close()
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(resp_body))
+	d.SetId(file)
+	return nil
 }
 
 func resourceUploadRead(d *schema.ResourceData, m interface{}) error {
@@ -96,5 +95,32 @@ func resourceUploadUpdate(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 func resourceUploadDelete(d *schema.ResourceData, m interface{}) error {
+	hostUrl := d.Get("host_url").(string)
+	filePath := d.Get("file_path").(string)
+	_, file := filepath.Split(filePath)
+
+	in := []byte(`{ "file":"" }`)
+	var raw map[string]string
+	json.Unmarshal(in, &raw)
+	raw["file"] = file
+	out, _ := json.Marshal(raw)
+	var jsonStr = []byte(out)
+	req, err := http.NewRequest("DELETE", hostUrl, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		log.Println("[ERROR] Error ", err)
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("[ERROR] Error while requesting Token ", err)
+	}
+	fmt.Println(resp.Status)
+	d.SetId("")
 	return nil
+
 }
